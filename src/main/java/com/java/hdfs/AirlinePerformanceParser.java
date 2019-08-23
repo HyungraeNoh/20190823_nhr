bc879b1882ae7ac8f0c1f6bb49e8948d6b4ffe75
GD7 <GD7@DESKTOP-QOLR3RJ>
2019-08-23 18:08:59 +0900

    10단위 재시험

---
+++ src/main/java/com/java/hdfs/AirlinePerformanceParser.java
@@ -0,0 +1,34 @@
+package com.java.hdfs;
+
+import org.apache.hadoop.io.Text;
+
+public class AirlinePerformanceParser {
+	private int year;
+	private int month;
+	private int departureDealyTime = 0;
+	private boolean departureDelayAvailable = true;
+	
+	public AirlinePerformanceParser(Text text) {
+		try {
+			String[] values = text.toString().split(",");
+			
+			if(!values[15].equals("NA")) {
+				departureDealyTime = Integer.parseInt(values[15]);
+			} else{
+				departureDelayAvailable = false;
+			}
+			
+			
+			year =  Integer.parseInt(values[0]);
+			
+			month =  Integer.parseInt(values[1]);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public int getYear() {return year;}
+	public int getMonth() {return month;}
+	public int getDepartureDealyTime() {return departureDealyTime;}
+	public boolean isDepartureDelayAvailable() {return departureDelayAvailable;}
+}
